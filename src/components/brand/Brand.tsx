1e53a6b8e81900ecf7be13ae258e930dac54ccce
Harsh2972004 <harshpreet2972004@gmail.com>
2023-06-09 13:33:34 +0530

    build brand section

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -1,7 +1,26 @@
 import "./brand.css";
+import { google, slack, dropbox, shopify, atlassian } from "./import";
 
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brand section-padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassain" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
