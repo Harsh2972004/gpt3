9b7c117554a572fb51557674630e132021a0f345
Harsh2972004 <harshpreet2972004@gmail.com>
2023-06-17 22:32:34 +0530

    build what gpt3 and features sections

--- src/components/feature/Feature.tsx
+++ src/components/feature/Feature.tsx
@@ -1,7 +1,22 @@
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+interface Props {
+  title: string;
+  text: string;
+}
+
+const Feature = ({ title, text }: Props) => {
+  return (
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container__feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
